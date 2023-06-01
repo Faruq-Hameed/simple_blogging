5d9bf8e1f6436e71955755d1823c743f7531b6c2
Faruq-Hameed <faruqhameed1@gmail.com>
2023-06-01 17:40:58 +0100

    worke on blog controller

--- src/controller/blogs.js
+++ src/controller/blogs.js
@@ -5,45 +5,47 @@ const {validatedBlogSchema} = require('../utils')
 /** post a new blog */
 const createBlog = async (req, res) => {
   try {
+    //validate the provided data
     const validation = validatedBlogSchema(req.body);
     if (validation.error) {
       res.status(422).send(validation.error.details[0].message);
       return;
     }
-    User.findById(req.body.postedBy).then((user) => {
-      if (!user) {
-        res.status(404).json({ message: "unknown user" });
-        return;
-      }
-      Blog.create({ ...req.body }) //create a new blog
-        .then((blog) => {
-          //adding the new blog id to the user's blogs list
-          user.blogs.push(blog._id);
-          user.save();
-          res
-            .status(200)
-            .json({ message: "blog created successfully", blog: blog });
-        });
-    });
-  } catch (err) {
+    //fetch the user making the blog post creation request
+    const user = await User.findById(req.body.postedBy);
+    if (!user) {
+      res.status(404).json({ message: "unknown user" });
+      return;
+    }
+    //create a new blog
+    const blog = await Blog.create({ ...req.body });        
+    user.blogs.push(blog._id); //adding the new blog id to the user's blogs list
+    user.save();
+
+    res.status(200).json({ message: "blog created successfully", blog });
+  } 
+  catch (err) {
     res.status(500).json({ message: err.message });
   }
 };
 
 /**Get all or some blogs controller */
-const getBlogs = async  (req, res)=>{
-    Blog.find().limit(2)
-     .then(blogs => {
-        if(blogs.length == 0){
-            res.status(204).send();
-            return;
-        }
-         res.status(200).json({message: "all blogs", blogs: blogs})
-     })
-     .catch(err =>{
-         res.status(404).json({message: err.message})
-     })
- }
+const getBlogs = async (req, res) => {
+  try {
+    const { limit } = req.query;
+    const blogs = await Blog.find()
+    .limit(limit * 1)
+    .populate('postedBy', {_id: 0, name: 1, email: 1})
+    .exec()
+    if( blogs.length < 0 ) {
+        return res
+        .status(200).json({message: "no blog available at this time"})
+    }
+    res.status(200).send(blogs);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
 
 /**Get a blog with the given id */
 const findBlogById = async (req, res)=>{
