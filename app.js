1ab5796a21b62ac81ab6612ac1f5182d1dda5610
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2023-08-02 21:25:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms - :remote-user :date"))
-app.use(helmet())
+app.use(helmet()) //security for response header
 
 
 //router level middlewares usage
